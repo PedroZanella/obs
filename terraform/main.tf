b5f7a068557e8eaf5dd61f8f539eb7e2a6c53ab4
PedroZanella <pedro_zanela@hotmail.com>
2025-11-28 00:38:51 -0300

    modificar make

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,12 +77,12 @@ resource "aws_security_group" "obs_sg" {
   }
 
   ingress {
-  description      = "HTTPS"
-  from_port        = 443
-  to_port          = 443
-  protocol         = "tcp"
-  cidr_blocks      = ["0.0.0.0/0"]
-}
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Grafana"
