fc94245b1ef414283bb719f973b7ea6f872bb2ad
PedroZanella <pedro_zanela@hotmail.com>
2025-11-27 23:47:17 -0300

    modified:   terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,6 +77,14 @@ resource "aws_security_group" "obs_sg" {
   }
 
   ingress {
+  description      = "HTTPS"
+  from_port        = 443
+  to_port          = 443
+  protocol         = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+  ingress {
     description = "Grafana"
     from_port   = 3300
     to_port     = 3300
